e660d933a3707342e351248deb34ba6f548708fb
Christoffer Lucas <christoffer.luccas@gmail.com>
2018-02-26 03:56:30 -0300

    resolvendo exercicios

--- math/AmountDuplicate/sol/Amount.py
+++ math/AmountDuplicate/sol/Amount.py
@@ -2,44 +2,103 @@
 https://www.codewars.com/kata/unknown-amount-of-duplicates-one-missing-number/train/python
 '''
 
-def find_min(arr):
-    min = arr[0]
-    for i in range(1, len(arr)):
-        if arr[i] < min :
-            min = arr[i]
-    return min
-
 '''
-    for i in range(0, len(possible_missing)):
-        flag = False
-        for j in arr:
-            if possible_missing[i] == j:
-                flag = True
-                break
-        if flag:
-            del possible_missing[i]
+TIME OUT
 '''
-
 def approachV1(arr):
-    arr.sort()
+    _map = {}
     repeated = []
     possible_missing = []
-    for i in range(0, len(arr) - 1):
-        if arr[i] == arr[i+1] and arr[i] not in repeated:
-            repeated.append(arr[i+1])
+    maxValue, minValue = arr[0], arr[0]
+    for i in arr:
+        if i > maxValue:
+            maxValue = i
+        if i < minValue:
+            minValue = i
+        if i not in _map:
+            _map[i] = 1
+        else:
+            _map[i] += 1
+            if i not in repeated:
+                repeated.append(i)
 
+    for i in range(minValue, maxValue + 1):
+        if i not in arr:
+            possible_missing.append(i)
+    repeated.sort()
+    return [possible_missing[0], repeated]
 
+'''
+TIME OUT
+'''
+def approachV2(arr):
+    _map = {}
+    repeated = []
+    maxValue, minValue = arr[0], arr[0]
+    acc = 0
+    for i in arr:
+        if i > maxValue:
+            maxValue = i
+        if i < minValue:
+            minValue = i
+        if i not in _map:
+            _map[i] = 1
+            acc += i
+        else:
+            _map[i] += 1
+            if i not in repeated:
+                repeated.append(i)
 
+    # quantos numeros temos na sequencia entre min e max inclusive ?
+    t = maxValue - minValue + 1
+    # soma dos numeros entre min e max inclusive - usando a formula para soma de termos de uma P.A
+    summation = t * (maxValue + minValue) // 2
+    repeated.sort()
+    return [summation - acc, repeated]
 
-    return [possible_missing, repeated]
+'''
+ACCEPTED
+'''
+def approachV3(arr):
+    _map = {}
+    maxValue, minValue = arr[0], arr[0]
+    acc = 0
+    for i in arr:
+        if i > maxValue:
+            maxValue = i
+        if i < minValue:
+            minValue = i
+        if i not in _map:
+            _map[i] = 1
+            acc += i
+        else:
+            _map[i] += 1
 
+    # quantos numeros temos na sequencia entre min e max inclusive ?
+    t = maxValue - minValue + 1
+    # soma dos numeros entre min e max inclusive - usando a formula para soma de termos de uma P.A
+    summation = t * (maxValue + minValue) // 2
+
+    flags = [False] * (maxValue - minValue + 1)
+    for k, v in _map.items():
+        if v > 1:
+            flags[k - minValue] = True
+    repeated = []
+    for i in range(0, len(flags)):
+        if flags[i]:
+            repeated.append(i + minValue)
+    return [summation - acc, repeated]
 
 def find_dups_miss(arr):
     # your code here
-    return approachV1(arr)
+    return approachV2(arr)
 
 
+print(find_dups_miss([500,501,503,503]))
+print(find_dups_miss([10,10,9,8,9,6,1,2,4,3,2,5,5,3]))
 print(find_dups_miss([10,9,8,9,6,1,2,4,3,2,5,5,3]))
+print(find_dups_miss([20,19,6,9,7,17,16,17,12,5,6,8,9,10,14,13,11,14,15,19]))
+print(find_dups_miss([24,25,34,40,38,26,33,29,50,31,33,56,35,36,53,49,57,27,37,40,48,44,32,35,45,52,43,47,26,51,55,28,41,42,46,51,25,30,44,54]))
 
 if __name__ == '__main__':
     pass
--- math/Polynomial2Coefficient/solution/Coefficient.py
+++ math/Polynomial2Coefficient/solution/Coefficient.py
@@ -2,5 +2,46 @@
 https://www.codewars.com/kata/polynomials-ii-coefficients-in-a-list/train/python
 '''
 
+
+# If polynomial degree == 3
+def calc_poly(pol_list, x):
+    cpol = []
+    q = len(pol_list)-1
+    #your code here
+    acc, idx2 = 0, q
+    for idx in range(q, -1, -1):
+        coef = pol_list[q-idx]
+        if coef is not 0:
+            acc += coef * (x ** idx)
+            copy_coef = coef if coef > 0 else -coef
+            if idx > 1:
+                pol = "{0}*x^{1}".format(copy_coef, idx) if copy_coef > 1 or copy_coef < 0 else "x^{0}".format(idx)
+            elif idx == 1:
+                pol = "{0}*x".format(copy_coef) if copy_coef > 1 or copy_coef < 0 else "x"
+            else:
+                pol = "{0}".format(copy_coef)
+
+            # adicionar sinal positivo a esquerda
+            if idx2 < q:
+                if coef > 0:
+                    pol = " + {0}".format(pol)
+                else:
+                    pol = " - {0}".format(pol)
+            # a nao ser que o primeiro coeficiente valido for negativo
+            elif coef < 0:
+                pol = "-{0}".format(pol)
+            cpol.append(pol)
+            idx2 -= 1
+
+    return "For {0} with x = {1} the value is {2}".format(''.join(cpol), x, acc)
+
+
+print(calc_poly([17, 2, 1, -33], 36))
+print(calc_poly([1, -18, 35, 79], -42))
+print(calc_poly([0, 5, 4], -58))
+print(calc_poly([6, 3, 5, -4], 4))
+print(calc_poly([2, 0, 5, -6, 4, 0], 2))
+print(calc_poly([-1, -6, 28, 79], 35))
+
 if __name__ == '__main__':
     pass
