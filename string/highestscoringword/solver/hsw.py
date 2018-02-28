b819b1fae2bc9a56ecd254ffeccb648302891744
chrislucas <christoffer.luccas@gmail.com>
2018-02-28 18:06:51 -0300

    exec

---
+++ string/highestscoringword/solver/hsw.py
@@ -0,0 +1,15 @@
+'''
+https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/train/python
+'''
+
+import string
+def high(x):
+    # Code here
+    ''.join(list(string.ascii_lowercase))
+
+    for str in x.split(" "):
+        pass
+
+
+if __name__ == '__main__':
+    pass
