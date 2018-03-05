d2c97c381b995fa967394eb5103bd2d8d9209b1c
Christoffer Lucas <christoffer.luccas@gmail.com>
2018-03-05 05:43:41 -0300

    estudos sobre regex

--- string/IPValidation/problem/ip_validation.py
+++ string/IPValidation/problem/ip_validation.py
@@ -1,10 +1,33 @@
+import re
+
 '''
 https://www.codewars.com/kata/515decfd9dcfc23bb6000006/train/python
 '''
 
-def is_valid_IP(strng):
-    return None
+pattern_ip1 = "(2[0-5]{2})|(1[0-9]{2})|([1-9][0-9])|([0-9])(\.((2[0-5]{2})|(1[0-9]{2})|([1-9][0-9])|([0-9]))){3}"
 
+
+def is_valid_IP(_str):
+    obj_match = re.match(pattern_ip1, _str)
+    if obj_match is not None:
+        print(obj_match)
+        _str_match = _str[obj_match.pos:obj_match.endpos]
+        print(_str_match)
+    return ''
+
+
+print(is_valid_IP('12.255.56.1'))
+print(is_valid_IP(''))
+print(is_valid_IP('abc.def.ghi.jkl'))
+print(is_valid_IP('123.456.789.0'))
+print(is_valid_IP('12.34.56'))
+print(is_valid_IP('12.34.56 .1'))
+print(is_valid_IP('12.34.56.-1'))
+print(is_valid_IP('123.045.067.089'))
+print(is_valid_IP('127.1.1.0'))
+print(is_valid_IP('0.0.0.0'))
+print(is_valid_IP('0.34.82.53'))
+print(is_valid_IP('192.168.1.300'))
 
 if __name__ == '__main__':
     pass
--- string/IPValidation/problem/regex_101.py
+++ string/IPValidation/problem/regex_101.py
@@ -2,6 +2,24 @@
 https://www.tutorialspoint.com/python/python_reg_expressions.htm
 '''
 
+'''
+Meta caracteres em regex . ^ $ * + ? { } [ ] \ | ( )
+
+() grupos
+
+$ corresponde ao final da string. Se adicionado a flag multiline \m ira corresponder com 
+as string apos o caracter de newline \n
+Exemplo $
+
+/r$/ corresponde com a string 'correr' mais nao corresponde com a string 'corre
+
+^ corresponde ao inicio do texto
+
+
+\b - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Regular_Expressions#special-word-boundary
+
+'''
+
 import re as regex
 
 
@@ -9,15 +27,34 @@ def test(pattern, _str):
     re_comp = regex.compile(pattern, regex.MULTILINE)
     result = re_comp.match(_str)
     if result is not None:
-        print(result)
-        print(result.re)
+        print("Result:\nStr: %s\n%s\n%s\n" % (_str, result.span(), result.re))
+        re_match = regex.match(pattern, _str)
+        if re_match is not None:
+            start, end = re_match.start(), re_match.end()
+            pos, endpos = re_match.pos, re_match.endpos
+            print("Match:\n%s\nStart=%d\nEnd=%d\nsubstr=%s\npe=%s\n"
+                  % (re_match, start, end, _str[start:end], _str[pos:endpos]))
+    else:
+        print("Nao foi encontrado o padrao %s no texto '%s'\n" % (pattern, _str))
 
 
+'''
+Corresponde a um numero que nao comeca com zero
+'''
+re_num_w_zeros = "^[1-9][0-9]*$"
+
+regex_ip = "[1-9][0-9]*(\.[1-9][0-9]*){1,3}"
+regex_ip_2 = "([1-2][0-5]{1,3}|[0-9])(\.([1-2][0-5]{1,3}|[0-9])){1,3}"
+regex_ip_3 = "(2[0-5]{2})|(1[0-9]{2})|([1-9][0-9])|([0-9])(\.((2[0-5]{2})|(1[0-9]{2})|([1-9][0-9])|([0-9]))){3}"
+
+test("[a-zA-Z]{1,}", "12321")
 test("[a-zA-Z]{1,}", "absc12321")
-test("(\d{1,3}\.){3}", "123.456.789")
+test("(\d{1,3}){3}", "123.456.789")
+test(regex_ip_2, "123.456.789.1")
+test(regex_ip_2, "0.0.0.0")
 
-
-
+test("[a-z]+r$", "correr")
+test("r$", "correr")
 
 if __name__ == '__main__':
     pass
