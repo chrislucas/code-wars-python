47b5f396de3df0e23e09432cccca2bdabcb24185
chrislucas <christoffer.luccas@gmail.com>
2018-06-07 18:35:27 -0300

    estudo

---
+++ algorithm/ds/SimpleMaze/solver/SimpleMaze.py
@@ -0,0 +1,21 @@
+'''
+https://www.codewars.com/kata/simple-maze
+
+['# ##',
+ '# k#',
+ '####']
+
+ True
+
+ ['####'.
+ '# k#',
+ '####']
+
+ False
+'''
+
+print(0xf)
+
+
+if __name__ == '__main__':
+    pass
