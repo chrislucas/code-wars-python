b84fec0f0e70b02b942a3f22de7472c36d3403f8
chrislucas <christoffer.luccas@gmail.com>
2018-07-24 18:15:05 -0300

    estudos

--- math/probability/SumInRollingCubicDice/sol/Solution.py
+++ math/probability/SumInRollingCubicDice/sol/Solution.py
@@ -5,10 +5,10 @@ https://www.codewars.com/kata/probabilities-for-sums-in-rolling-cubic-dice/train
 
 def _combination(arr, chosen, s, e, c, p, _sum):
     if c == p:
-        acc = 0
+        acc2 = 0
         for i in range(0, p):
-            acc += arr[chosen[i]]
-        return 1 if acc == _sum else 0
+            acc2 += arr[chosen[i]]
+        return 1 if acc2 == _sum else 0
     acc = 0
     for i in range(s, e):
         chosen[c] = i
@@ -25,11 +25,11 @@ def combinaroty(arr, _sum, p):
 def rolldice_sum_prob(_sum, dice_amount):
     # your code here
     a = combinaroty([1, 2, 3, 4, 5, 6], _sum, dice_amount)
-
     # prob = probability of having the sum value when rolling dice
     return a / (6 ** dice_amount)
 
 
+print(rolldice_sum_prob(3, 3))
 print(rolldice_sum_prob(8, 3))
 print(rolldice_sum_prob(11, 2))
 print(rolldice_sum_prob(8, 2))
