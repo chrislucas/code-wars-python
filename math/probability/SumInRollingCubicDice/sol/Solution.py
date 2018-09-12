75f8aaf90f6a56cda6c3000338d64c665cce09a6
chrislucas <christoffer.luccas@gmail.com>
2018-09-12 16:20:21 -0300

    estudo

--- math/probability/SumInRollingCubicDice/sol/Solution.py
+++ math/probability/SumInRollingCubicDice/sol/Solution.py
@@ -1,6 +1,7 @@
 '''
 https://www.codewars.com/kata/probabilities-for-sums-in-rolling-cubic-dice/train/python
 '''
+from builtins import print
 
 
 def _combination(arr, chosen, s, e, c, p, _sum):
@@ -8,7 +9,22 @@ def _combination(arr, chosen, s, e, c, p, _sum):
         acc2 = 0
         for i in range(0, p):
             acc2 += arr[chosen[i]]
-        return 1 if acc2 == _sum else 0
+            #print("%d " % arr[chosen[i]], end='')
+        #print("")
+        flag = True
+        for a in range(0, len(chosen)-1):
+            if chosen[a] - chosen[a+1] != 0:
+                flag = False
+                break
+        if acc2 == _sum:
+            if flag:
+                return 1
+            else:
+                acc = 1
+                for x in range(len(chosen), 0, -1):
+                    acc *= x
+                return acc
+        return 0
     acc = 0
     for i in range(s, e):
         chosen[c] = i
@@ -29,10 +45,10 @@ def rolldice_sum_prob(_sum, dice_amount):
     return a / (6 ** dice_amount)
 
 
-print(rolldice_sum_prob(3, 3))
-print(rolldice_sum_prob(8, 3))
+#print(rolldice_sum_prob(3, 3))
 print(rolldice_sum_prob(11, 2))
-print(rolldice_sum_prob(8, 2))
+print(rolldice_sum_prob(8, 3))
+#print(rolldice_sum_prob(8, 2))
 
 if __name__ == '__main__':
     pass
